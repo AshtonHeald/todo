ff404b821e48c3904db9081b3082f054dc234297
Ashton Heald <devtheash@gmail.com>
2024-04-29 14:15:37 -0500

    edited header icon size

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
 	return (
 		<header className="flex items-center justify-between w-auto min-h-16 p-4">
 			<div className="mr-4">
-				<CheckCircle size={24} />
+				<CheckCircle size={32} />
 			</div>
 			<div className="flex-1">
 				<h1 className="font-bold text-3xl text-center">
